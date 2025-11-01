80c0b96f78db656fdedaeb995f8ab988abb28f87
Patrick <patrickreyes0625@gmail.com>
2025-11-01 14:27:31 +0800

    Adding of HomePage and changing route

---
+++ resources/views/home.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=hi, initial-scale=1.0">
+    <title>Home page</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+<body>
+    <h1>Hello Patrick pakyu</h1>
+
+</body>
+</html>
